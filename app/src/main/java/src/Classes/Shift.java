e82085bc9d5547115bdbc84cf1b6c6eb869ccd9b
nadav1996700 <nadav1996700@gmail.com>
2020-09-17 01:18:36 +0300

    create classes

--- app/src/main/java/src/Classes/Shift.java
+++ app/src/main/java/src/Classes/Shift.java
@@ -1,4 +1,31 @@
 package src.Classes;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Shift {
+    private Date date;
+    private ArrayList<Worker> morning_shift;
+    private ArrayList<Worker> evening_shift;
+    private ArrayList<Worker> night_shift;
+
+    public Shift(Date date) {
+        this.date = date;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public ArrayList<Worker> getMorning_shift() {
+        return morning_shift;
+    }
+
+    public ArrayList<Worker> getEvening_shift() {
+        return evening_shift;
+    }
+
+    public ArrayList<Worker> getNight_shift() {
+        return night_shift;
+    }
 }
--- app/src/main/java/src/Classes/Worker.java
+++ app/src/main/java/src/Classes/Worker.java
@@ -8,8 +8,93 @@ public class Worker {
     private String password;
     private String id;
     private String phone;
-    private int age;
     private String company;
+    private int age;
     private int photo;
 
+    public Worker(String first_name, String last_name, String username, String password, String id,
+                  String phone, String company, int age, int photo) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.username = username;
+        this.password = password;
+        this.id = id;
+        this.phone = phone;
+        this.age = age;
+        this.company = company;
+        this.photo = photo;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    @Override
+    public String toString() {
+        return first_name + " " + last_name;
+    }
 }
